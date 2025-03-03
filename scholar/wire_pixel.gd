dab57c02258fbc7483700466c37e10fea1d798db
chase <chase.entwistle@gmail.com>
2025-03-03 14:10:10 -0600

    Demo version

--- scholar/wire_pixel.gd
+++ scholar/wire_pixel.gd
@@ -5,8 +5,17 @@ var depth = 0
 
 var blockable = true
 var blocked = false
-var active = true
+var active = false
 
+static var allofus = []
+static var first = null
+static var last
+func _ready() -> void:
+	allofus.append(self)
+	if first == null:
+		first = self
+	last = self
+	
 # Signal handler for when a body enters the area
 func _on_body_entered(body):
 	if blockable:
@@ -24,15 +33,25 @@ func set_as_root():
 	blockable = false
 	$MeshInstance3D.visible = false
 
+var holder = null
+static var id = 0
 func update():
-	var was = active
-	if blockable:
-		active = false
-		if !blocked:
-			for pix in neighbors:
-				if pix.active:
-					active = true
-	
-	$MeshInstance3D.visible = active
-	if was != active:
-		neighbors.all(update)
+	for pix in allofus:
+		pix.active = false
+	first.spread()
+	for pix in allofus:
+		pix.get_node("MeshInstance3D").visible = pix.active
+
+	holder.get_parent().get_node("Painting").visible = !last.active
+
+func spread():
+	if !blocked:
+		active = true
+		for pix in neighbors:
+			if !pix.active:
+				pix.spread()
+
+func addNeighbor(pix):
+	neighbors.append(pix)
+	pix.neighbors.append(self)
+	update()
