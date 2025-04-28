3b2a3f664fe743cc83f0d77260780ad167fd3a6d
ddavis-4 <123651199+ddavis-4@users.noreply.github.com>
2025-04-28 00:11:16 -0500

    this should not beak the game

--- hammer.gd
+++ hammer.gd
@@ -1,22 +1,10 @@
-extends MeshInstance3D
+extends RigidBody3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_rigid_body_3d_body_entered(body: Node) -> void:
-	# check if the nodes we collide with are in breakable group
-	var group = body.get_parent()
-	print(group)
-	if group.is_in_group("breakable"):
-		if group.has_method("break_apart"):
-			group.break_apart() 
-	queue_free()
-	pass # Replace with function body.
+func _on_body_entered(body: Node) -> void:
+	print("Hammer hit:", body.name)
+	
+	# Check the parent (Pillar Node3D)
+	var parent = body.get_parent()
+	if parent and parent.is_in_group("breakable") and parent.has_method("break_apart"):
+		print("It's breakable! Breaking now.")
+		parent.break_apart()
